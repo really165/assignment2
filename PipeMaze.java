bb4c445415dddb6a31f08fe8f2628e03a54d3174
really165 <31826004+really165@users.noreply.github.com>
2018-10-26 13:01:12 -0600

    can now find which moves are available

--- PipeMaze.java
+++ PipeMaze.java
@@ -1,5 +1,44 @@
 package assignment2;
 
 public class PipeMaze {
+    //position in maze
+    int x, y;
+    //current maze
+    char[][] current;    
+    //booleans to keep track of which ways can be moved
+    boolean up, down, left, right;
     
+    
+    public PipeMaze(char[][] input){
+        this.current = input;
+    }
+    
+    public void setPosition(int xPos, int yPos){
+        this.x = xPos;
+        this.y = yPos;
+    }
+    
+    public void setAvailableMoves(){
+        //finds available moves
+        if(x-1>=0){
+            if(current[(x-1)][y]=="_".charAt(0)){
+                up = true;
+            }
+        }
+        if(x+1 < current.length){
+            if(current[(x+1)][y]=="_".charAt(0)){
+                down = true;
+            }
+        }
+        if(y-1>=0){
+            if(current[x][(y-1)]=="_".charAt(0)){
+                left = true;
+            }
+        }
+        if(y+1 < current[0].length){
+            if(current[x][(y+1)]=="_".charAt(0)){
+                right = true;
+            }
+        }
+    }
 }
